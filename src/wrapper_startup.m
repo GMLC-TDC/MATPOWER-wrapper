0c1d136f7b93d334b2d6511181020d974568be1b
JacobHastings <107068568+JacobHastings@users.noreply.github.com>
2023-05-15 13:58:28 -0700

    Update wrapper_startup.m

--- src/wrapper_startup.m
+++ src/wrapper_startup.m
@@ -2,11 +2,17 @@
 1;
 %% add MATPOWER paths
 
+<<<<<<< Updated upstream
 if exist('OCTAVE_VERSION', 'builtin') ~= 0
  MATPOWER_start_up_path = '/home/helics-user/Projects/matpower7.1/startup.m';
  end
 ##MATPOWER_start_up_path = 'C:\Users\mukh915\matpower8.0b1\startup.m';
+=======
+% MATPOWER_start_up_path = '/home/helics-user/Projects/HELICS_Plus/matpower7.1/matpower_startup.m';
+% MATPOWER_start_up_path = 'C:\Users\mukh915\matpower8.0b1\startup.m';
+>>>>>>> Stashed changes
 % MATPOWER_start_up_path = 'C:\Users\jw.hastings\Documents\matpower7.1\startup.m';
+MATPOWER_start_up_path = '/Users/jacobhastings/Desktop/WSU-Grad/Lab/Matpower/matpower7.1/startup.m';
 
 run(MATPOWER_start_up_path)
 
@@ -16,6 +22,7 @@ run(MATPOWER_start_up_path)
 % libraryName = 'C:\Users\jw.hastings\OneDrive - Washington State University (email.wsu.edu)\Documents\matHELICS\matHELICS\helics.dll';
 % headerName = 'C:\Users\jw.hastings\OneDrive - Washington State University (email.wsu.edu)\Documents\matHELICS\matHELICS\helics_minimal.h';
 
+<<<<<<< Updated upstream
 ##libraryName = 'C:\Users\mukh915\matHELICS\helics\helics.dll';
 ##headerName = 'C:\Users\mukh915\matHELICS\helics\include\helics_minimal.h';
 
@@ -26,4 +33,18 @@ run(MATPOWER_start_up_path)
 %% add Mosek paths
 ##addpath('C:\Program Files\Mosek\10.0\toolbox\r2017a')
 ##addpath('C:\Program Files\Mosek\10.0\toolbox\r2017aom')
+=======
+% libraryName = 'C:\Users\mukh915\matHELICS\helics\helics.dll';
+% headerName = 'C:\Users\mukh915\matHELICS\helics\include\helics_minimal.h';
+
+% helicsStartup(libraryName, headerName)
+% addpath('C:\Users\mukh915\matHELICS\helics\')
+% addpath('C:\Users\jw.hastings\OneDrive - Washington State University (email.wsu.edu)\Documents\matHELICS\matHELICS')
+
+%% add Mosek paths
+% addpath('C:\Program Files\Mosek\10.0\toolbox\r2017a')
+% addpath('C:\Program Files\Mosek\10.0\toolbox\r2017aom')
+% addpath('/Users/jacobhastings/Desktop/WSU-Grad/Lab/MOSEK/mosek/10.0/toolbox/r2017a')
+% addpath('/Users/jacobhastings/Desktop/WSU-Grad/Lab/MOSEK/mosek/10.0/toolbox/r2017aom')
+>>>>>>> Stashed changes
 
