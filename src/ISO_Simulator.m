27e64aed687b4b726141f5fcd4854774dff14738
JacobHastings <107068568+JacobHastings@users.noreply.github.com>
2023-08-16 13:27:57 -0700

    Revert "Merge branch 'master' of https://github.com/GMLC-TDC/MATPOWER-wrapper"
    This reverts commit ae2b50f07be0cc7630ff1df84de5946d6ec652df, reversing
    changes made to 158a07b641e13b46acb6d4b9ef22d98e3dc9763c.

--- src/ISO_Simulator.m
+++ src/ISO_Simulator.m
@@ -11,7 +11,7 @@ DAM_plot_option = 0;
 isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
 %% Load Model
 wrapper_startup;
-Wrapper = MATPOWERWrapper('wrapper_config_test.json', isOctave);
+Wrapper = MATPOWERWrapper('wrapper_config.json', isOctave);
 
 %% Read profile and save it within a strcuture called load
 if isOctave
@@ -52,14 +52,12 @@ Wrapper =  Wrapper.update_model(); % Do this to get the new limits into the the 
 %% Adding Zonal Reserves %%
 res_zones = Wrapper.mpc.bus(:, 11);
 max_zonal_loads =  [19826.18, 25282.32, 19747.12, 6694.77]; % Based on 2016 data
-max_zonal_loads =  [71590]; % Test Case Based on 2016 data
 % Assuming reserve requirement to be 2 % of peak load
-zonal_res_req = max_zonal_loads'*1/100; 
+zonal_res_req = max_zonal_loads'*2.5/100; 
 % assuming Non VRE generators to participate in reserve allocations
 reserve_genId = [1:33];
-reserve_genId = [1:13]; %% Test case
 % assuming 5% reserve availiability from all generators
-reserve_genQ = Wrapper.mpc.gen(reserve_genId, 9)* 5/100; 
+reserve_genQ = Wrapper.mpc.gen(reserve_genId, 9)* 7.5/100; 
 % assuming constant price for reserves from all generators
 reserve_genP = 1*ones(length(reserve_genId), 1);
 Wrapper.MATPOWERModifier = Wrapper.MATPOWERModifier.add_zonal_reserves(reserve_genId, reserve_genQ, reserve_genP, zonal_res_req);
@@ -314,7 +312,6 @@ while time_granted < Wrapper.duration
         xgd_table.colnames = { 'CommitKey' };
         xgd_table.data = 1*ones(size(mpc_mod.gen, 1),1);
         must_run_idx = [75:110]; %% Nuclear + VRE
-        must_run_idx = [10:18]; %% for Test case 
         xgd_table.data(must_run_idx) = 2;
         xgd = loadxgendata(xgd_table, mpc_mod);
         xgd.PositiveLoadFollowReserveQuantity =  mpc_mod.gen(:,17)*60;
@@ -347,9 +344,9 @@ while time_granted < Wrapper.duration
         
         fprintf('Wrapper: Running DA Market at Time %s\n', (datestr(datenum(Wrapper.config_data.start_time) + (time_granted/86400))))
         % mpopt = mpoption('verbose', 1, 'out.all', 1, 'most.dc_model', 0, 'opf.dc.solver','GLPK');
-        mpopt = mpoption('verbose', 0, 'out.all', 0, 'most.dc_model', 1);
+        mpopt = mpoption('verbose', 1, 'out.all', 0, 'most.dc_model', 1);
         mpopt.mips.max_it = 200;
-        mpopt = mpoption(mpopt, 'most.uc.run', 1);
+        mpopt = mpoption(mpopt, 'most.uc.run', 0);
         mdo = most(mdi, mpopt);
         
         %% Storing DAM Results %%
