6df2952686ff1934a5d18ba19798866df0ffad25
mukh915 <monish.mukherjee@pnnl.gov>
2024-04-19 11:26:18 -0700

    fine tuning for final runs v2

--- post_processing/plot_gen_mix.m
+++ post_processing/plot_gen_mix.m
@@ -5,9 +5,10 @@ close all
 
 
 case_data = read_json('../src/wrapper_config_v2.json');
-casename = 'Base';
-month = 'February 2016';
+casename = 'Base_new';
+month = 'August 2021';
 start_time = datetime(case_data.start_time);
+end_time = datetime(case_data.end_time);
 % start_time = datetime('2016-08-01');
 
 model_file = strcat(case_data.matpower_most_data.datapath, case_data.matpower_most_data.case_name);
@@ -28,111 +29,92 @@ set(gca, 'FontName', 'Times New Roman')
 grid on 
 
 
+%% Generation Mix Plotting
 
 
-gen_mix_type = {'nuclear','coal','gas_cc','gas','wind'};
-newcolors = [0.5 0.5 0.5; 0 0 0; 1 .65 0; 1 0.58 0; 0 0 1];
+%% Month of August
+input_file_name = strcat(case_data.matpower_most_data.datapath, '2021_Aug_ERCOT_Gen_Mix.csv');
+gen_mix = readtable(input_file_name);
+load_data_t = readtable(strcat('../outputs/',casename,'_RTM_PD.csv'));
+total_load_t = sum(load_data_t{:, 2:end},2);
 
-%% Creating Gen Mix Category %%
-% for i = 1:length(model_data.genfuel)
-%     genfuel{i,1} = model_data.genfuel{i}{1};
-%      if strfind(model_data.genfuel{i}{2}, 'Combined Cycle')
-%          genfuel{i,1} = strcat(genfuel{i,1}, '_cc');
-%      end
-%     
-% end
+UTC_adjust = 1.25;
+frm = find(gen_mix.Datetime == start_time + hours(UTC_adjust));
+to = find(gen_mix.Datetime == end_time+ hours(UTC_adjust));
 
-gen_mix_type = {'nuclear', 'hydro', 'coal','ng', 'wind', 'solar'};
-newcolors =   [0.5 0.5 0.5; 0.4940 0.1840 0.5560; 0 0 0; 1 .58 0; 0 0 1; 1 1 0];
+
+
+case_file_PG = strcat('../outputs/',casename,'_RTM_PG.csv');
+case_data_PG = readtable(case_file_PG);
+
+
 for i = 1:length(model_data.genfuel)
     genfuel{i,1} = model_data.genfuel{i};    
 end
 
-
-% gen_mix_type = unique(genfuel);
-
+gen_mix_type = {'nuclear',    'coal',     'hydro',       'solar',  'ng', 'wind',};
+newcolors =   [0.5 0.5 0.5; 0 0 0; 0.4940 0.1840 0.5560; 1 1 0; ; 1 .58 0; 0 0 1];
 gen_idx = struct()
 for type_idx = 1:length(gen_mix_type)
     type = gen_mix_type(type_idx);
     gen_idx.(type{1}) = find(strcmp(genfuel, type{1}));
 end
 
-    
-%% Reading OPF Solutions %%
-
-case_file_PG = strcat('../outputs/',casename,'_RTM_PG.csv');
-case_data_PG = readmatrix(case_file_PG);
-
-
-time_data = start_time + seconds(case_data_PG(:,1));
-
+gen_dispatch = array2table(zeros(height(case_data_PG), length(gen_mix_type)),'VariableNames', gen_mix_type);
 for type_idx = 1:length(gen_mix_type)
     type = gen_mix_type(type_idx);
     gen_type_indexes = gen_idx.(type{1})+1; %% adding +1 for as the data is moved by +1 due to time 
-    gen_mix_data(:, type_idx) = sum(case_data_PG(:, gen_type_indexes), 2);
+    gen_dispatch.(type{1}) = sum(case_data_PG{:, gen_type_indexes}, 2);
     Legend{type_idx}=type{1};
 end
 
+%% Figure 1
+y_sim = [];
+idx = 1;
+for k = 1:height(case_data_PG)
+    x = [gen_dispatch.nuclear(idx), gen_dispatch.coal(idx), gen_dispatch.hydro(idx) , gen_dispatch.solar(idx), gen_dispatch.ng(idx), ...
+        gen_dispatch.wind(idx)];
+    idx = idx + 1;
+    y_sim = [y_sim; x];
+end
 
-figure
-a=axes();
-set(a,'Units','normalized');
-area(time_data',gen_mix_data, 'LineWidth',1.)
+figure,
+subplot(2,1,1);
+area(time_data, y_sim, 'LineStyle', 'none')
 colororder(newcolors)
-legend(Legend)
-a.YAxis.Exponent = 0;
-ylabel(a,'Net Generation (MW)','FontSize',14);
-set(gca, 'FontName', 'Times New Roman')
-xlabel(a,'Time of Day (Hr)','FontSize',14)
-set(gca, 'FontName', 'Times New Roman')
-xlim([time_data(1),time_data(end)])
-grid on 
-title('Simulated ERCOT Gen Mix from Wrapper')
+hold on
+plot(time_data, total_load_t, 'k.', 'LineWidth', 1.5);
+set(gca, 'YTickLabel',get(gca,'YTick'))
+lgnd = legend({'Nuclear', 'Coal', 'Hydro', 'Solar', 'Natural Gas', 'Wind', 'Total Load'});
+xlabel('Time')
+ylabel('Generation (MW)')
+title('MATPOWER Simulation (Aug.)')
+xlim([time_data(1), time_data(end)])
+xticks(time_data(288:288*2:end))
 
-% input_file_name = strcat(case_data.matpower_most_data.datapath, 'ERCOTGenByFuel2016.xlsx');
-% ERCOT_actual_gen_mix_data_raw = readtable(input_file_name,'Sheet', month, 'Format','auto');
-% ERCOT_actual_gen_mix_data_raw.Time.Format = 'yyyy-MM-dd hh:mm';
-% ERCOT_actual_gen_mix_data = ERCOT_actual_gen_mix_data_raw(isbetween(ERCOT_actual_gen_mix_data_raw.Time, time_data(1), time_data(end)), :);
-% vars = ERCOT_actual_gen_mix_data.Properties.VariableNames;
-% for type_idx = 1:length(gen_mix_type)    
-%     type = gen_mix_type(type_idx);
-%     type_idx_actual_data(type_idx) =  find(strcmp(lower( vars ), type{1}));
-% end
+%% Figure 2
+y_actual = [];
+for k = frm : to
+    x = [gen_mix.Nuclear(k), gen_mix.Coal(k), gen_mix.Hydro(k), gen_mix.Solar(k), ...
+        gen_mix.Gas_CC(k)+gen_mix.Gas(k), gen_mix.Wind(k)];
+    y_actual = [y_actual; 4*x];
+end
 
-input_file_name = strcat(case_data.matpower_most_data.datapath, '2021_Aug_ERCOT_Gen_Mix.csv');
-ERCOT_actual_gen_mix_data_raw = readtable(input_file_name, 'Format','auto');
-ERCOT_actual_gen_mix_data_raw.Datetime.Format = 'yyyy-MM-dd HH:MM';
-ERCOT_actual_gen_mix_data = ERCOT_actual_gen_mix_data_raw(isbetween(ERCOT_actual_gen_mix_data_raw.Datetime, time_data(1), time_data(end)), :);
+time_actual = gen_mix.Datetime(frm: to) - hours(UTC_adjust);
 
 
-Vars = {'Coal', 'Hydro', 'Nuclear', 'ng', 'Wind', 'Solar', 'Others'}
-ERCOT_actual_data_select_gen_type = array2table(zeros(height(ERCOT_actual_gen_mix_data),7), 'VariableNames', Vars );
-
-datetime =  ERCOT_actual_gen_mix_data.Datetime;
-
-ERCOT_actual_data_select_gen_type.Nuclear  =  ERCOT_actual_gen_mix_data.Nuclear;
-ERCOT_actual_data_select_gen_type.Hydro    =  ERCOT_actual_gen_mix_data.Hydro;
-ERCOT_actual_data_select_gen_type.Coal     =  ERCOT_actual_gen_mix_data.Coal;
-ERCOT_actual_data_select_gen_type.ng       =  ERCOT_actual_gen_mix_data.Gas_CC + ERCOT_actual_gen_mix_data.Gas;
-ERCOT_actual_data_select_gen_type.Wind     =  ERCOT_actual_gen_mix_data.Wind;
-ERCOT_actual_data_select_gen_type.Solar    =  ERCOT_actual_gen_mix_data.Solar;
-ERCOT_actual_data_select_gen_type.Others    =  ERCOT_actual_gen_mix_data.Other + ERCOT_actual_gen_mix_data.Biomass;
-
-figure
-a=axes();
-set(a,'Units','normalized');
-area(datetime,ERCOT_actual_data_select_gen_type{:,:}*4, 'LineWidth',1.25)
+subplot(2,1,2);
+area(time_actual, y_actual, 'LineStyle', 'none');
 colororder(newcolors)
-legend(Legend)
-ylabel(a,'Net Generation (MW)','FontSize',14);
-a.YAxis.Exponent = 0;
-set(gca, 'FontName', 'Times New Roman')
-xlabel(a,'Time of Day (Hr)','FontSize',14)
-xlim([datetime(1),datetime(end)])
-set(gca, 'FontName', 'Times New Roman')
-grid on 
-title('Actual ERCOT Gen Mix from Settlement Data')
-
+hold on
+plot(time_data, total_load_t, 'k.', 'LineWidth', 1.5);
+set(gca, 'YTickLabel',get(gca,'YTick'))
+lgnd = legend({'Nuclear', 'Coal', 'Hydro', 'Solar', 'Natural Gas', 'Wind', 'Total Load'});
+xlabel('Time')
+ylabel('Generation (MW)')
+title('Actual ERCOT Dispatch (Aug.)')
+xlim([time_data(1), time_data(end)])
+xticks(time_data(288:288*2:end))
 
 function val = read_json(file)
        fid = fopen(file); 
--- post_processing/real_price_compare.m
+++ post_processing/real_price_compare.m
@@ -2,43 +2,166 @@ clc
 clear all
 close all
 
-Cases = {'Base','Flex10','Flex20','Mis10','Mis20'};
+addpath ('../src/')
+wrapper_startup;
+Wrapper = MATPOWERWrapper('../src/wrapper_config_v2.json', 0);
+Wrapper = Wrapper.read_profiles('load_profile_info', 'load_profile');
+load_total = transpose(sum(transpose(Wrapper.profiles.load_profile(:,2:9))));
+
+
+% Cases = {'Base','Flex10','Flex20','Mis10','Mis20'};
+Cases = {'Base_new'};
 fid = fopen('../src/wrapper_config_v2.json'); 
 raw = fread(fid,inf); 
 str = char(raw'); 
 fclose(fid); 
 case_data = jsondecode(str);
 start_time = datetime(case_data.start_time);
+end_time = datetime(case_data.end_time);
 start_day = 5;
 
-for i=1:length(Cases)
-    casename = Cases(i);
-    case_file_LMP = strcat('../outputs/',char(casename),'_RTM_LMP.csv');
-    case_data_LMP(i,:,:) = readmatrix(case_file_LMP);
-    case_file_PG = strcat('../outputs/',char(casename),'_RTM_PG.csv');
-    case_data_PG(i,:,:) = readmatrix(case_file_PG);
-    case_file_PD = strcat('../outputs/',char(casename),'_RTM_PD.csv');
-    case_data_PD(i,:,:) = readmatrix(case_file_PD);
 
-    case_file_LMP_DAM = strcat('../outputs/',char(casename),'_DAM_LMP.csv');
-    case_data_LMP_DAM_raw(i,:,:) = readmatrix(case_file_LMP_DAM);
-    case_file_PG_DAM = strcat('../outputs/',char(casename),'_DAM_PG.csv');
-    case_data_PG_DAM_raw(i,:,:) = readmatrix(case_file_PG_DAM);
-    case_file_PD_DAM = strcat('../outputs/',char(casename),'_DAM_PD.csv');
-    case_data_PD_DAM_raw(i,:,:) = readmatrix(case_file_PD_DAM);
+for i = 1:length(load_total)
+    load_time(i) = start_time + (Wrapper.profiles.load_profile(i,1)/86400);
 end
+
+% for i=1:length(Cases)
+%     casename = Cases(i);
+%     case_file_LMP = strcat('../outputs/',char(casename),'_RTM_LMP.csv');
+%     case_data_LMP(i,:,:) = readmatrix(case_file_LMP);
+%     case_file_PG = strcat('../outputs/',char(casename),'_RTM_PG.csv');
+%     case_data_PG(i,:,:) = readmatrix(case_file_PG);
+%     case_file_PD = strcat('../outputs/',char(casename),'_RTM_PD.csv');
+%     case_data_PD(i,:,:) = readmatrix(case_file_PD);
+% 
+%     case_file_LMP_DAM = strcat('../outputs/',char(casename),'_DAM_LMP.csv');
+%     case_data_LMP_DAM_raw(i,:,:) = readmatrix(case_file_LMP_DAM);
+%     case_file_PG_DAM = strcat('../outputs/',char(casename),'_DAM_PG.csv');
+%     case_data_PG_DAM_raw(i,:,:) = readmatrix(case_file_PG_DAM);
+%     case_file_PD_DAM = strcat('../outputs/',char(casename),'_DAM_PD.csv');
+%     case_data_PD_DAM_raw(i,:,:) = readmatrix(case_file_PD_DAM);
+% end
+
+
+casename = Cases(1);
+case_file_LMP = strcat('../outputs/',char(casename),'_RTM_LMP.csv');
+case_data_LMP = readmatrix(case_file_LMP);
+case_file_PG = strcat('../outputs/',char(casename),'_RTM_PG.csv');
+case_data_PG= readmatrix(case_file_PG);
+case_file_PD = strcat('../outputs/',char(casename),'_RTM_PD.csv');
+case_data_PD = readmatrix(case_file_PD);
+
+case_file_LMP_DAM = strcat('../outputs/',char(casename),'_DAM_LMP.csv');
+case_data_LMP_DAM_raw = readmatrix(case_file_LMP_DAM);
+case_file_PG_DAM = strcat('../outputs/',char(casename),'_DAM_PG.csv');
+case_data_PG_DAM_raw = readmatrix(case_file_PG_DAM);
+case_file_PD_DAM = strcat('../outputs/',char(casename),'_DAM_PD.csv');
+case_data_PD_DAM_raw = readmatrix(case_file_PD_DAM);
+
 
 real_prices_file_DA = strcat('../system_data/','2021_ERCOT_DA_prices_Aug.csv');
 real_prices_file_RT = strcat('../system_data/','2021_ERCOT_RT_prices_Aug.csv');
-real_prices_data_DA = readmatrix(real_prices_file_DA);
-real_prices_data_RT = readmatrix(real_prices_file_RT);
+real_prices_data_DA = readtable(real_prices_file_DA);
+real_prices_data_RT = readtable(real_prices_file_RT);
+real_prices_data_table_DA = readtable(real_prices_file_DA);
+real_prices_data_table_RT = readtable(real_prices_file_RT);
 
-start_index = 96*(start_day-1) - 4;
-for i=start_index:start_index+(96*5)
-    max_real_LMP(i-start_index+1) = max(transpose(real_prices_data_RT(i,2:16)));
-    min_real_LMP(i-start_index+1) = min(transpose(real_prices_data_RT(i,2:16)));
+UTC_adjust = 2.0;
+idx_time_RT = (real_prices_data_table_RT.Time >= start_time+ hours(UTC_adjust)) & (real_prices_data_table_RT.Time < end_time+ hours(UTC_adjust));
+idx_time_DA = (real_prices_data_table_DA.Time >= start_time+ hours(UTC_adjust)) & (real_prices_data_table_DA.Time < end_time+ hours(UTC_adjust));
+% idx_load = transpose(find(start_time_load<=load_time & load_time<=end_time_load));
+
+
+real_prices_data_DA_interval = real_prices_data_table_DA(idx_time_DA,:);
+real_prices_data_RT_interval = real_prices_data_RT(idx_time_RT,:);
+
+real_prices_max_DA = max(real_prices_data_DA_interval{:,2:end}');
+real_prices_min_DA = min(real_prices_data_DA_interval{:,2:end}');
+real_prices_max_RT = max(real_prices_data_RT_interval{:,2:end}');
+real_prices_min_RT = min(real_prices_data_RT_interval{:,2:end}');
+
+% start_index_RT = 96*(start_day-1) - 4;
+% for i=start_index_RT:(start_index_RT+(96*7))-1
+%     max_real_LMP_RT(i-start_index_RT+1) = max(real_prices_data_RT(i,2:16));
+%     min_real_LMP_RT(i-start_index_RT+1) = min(real_prices_data_RT(i,2:16));
+%     time_real_RT(i-start_index_RT+1) = start_time + ((i-start_index_RT)/96);
+% end
+% 
+% start_index_DA = 24*(start_day-1);
+% for i=start_index_DA:(start_index_DA + (24*7))-1
+%     max_real_LMP_DA(i-start_index_DA+1) = max(real_prices_data_DA(i,2:16));
+%     min_real_LMP_DA(i-start_index_DA+1) = min(real_prices_data_DA(i,2:16));
+%     time_real_DA(i-start_index_DA+1) = start_time + ((i-start_index_DA)/24);
+% end
+
+for i=1:length(case_data_LMP(:,1))
+    LMP_time_RT(i) = start_time + (case_data_LMP(i,1)/86400);
+end
+for i = 1:length(case_data_LMP_DAM_raw(:,1))
+    LMP_time_DA(i) = start_time + (case_data_LMP_DAM_raw(i,1)/86400);
 end
 
+actual_time_RT = real_prices_data_RT_interval.Time - hours(UTC_adjust);
+actual_time_DA = real_prices_data_DA_interval.Time - hours(UTC_adjust);
+
+%% Plot Figures
+figure()
+subplot(2,1,1)
+plot(actual_time_RT,real_prices_min_RT, 'LineWidth',1.25)
+hold on
+plot(actual_time_RT,real_prices_max_RT, 'LineWidth',1.25)
+plot(LMP_time_RT,mean(case_data_LMP(:,2:9),2), 'k', 'LineWidth',1.5)
+% plot(LMP_time_RT,mean(case_data_LMP(:,2:end),2), 'k', 'LineWidth',1.5)
+legend({'Actual (minimum)','Actual (maximum)','Simulation (Average)'})
+ylabel('LMP($/MWHr)')
+title('Actual Versus Simulated LMPs: Real Time')
+grid on
+% ylim([0,80])
+% yyaxis right
+% plot(LMP_time_RT,sum(case_data_PD(:,2:end),2))
+% title('Actual Versus Simulated LMPs: Day Ahead')
+% hold off
+
+subplot(2,1,2)
+plot(actual_time_DA,real_prices_min_DA, 'LineWidth',1.25)
+hold on
+plot(actual_time_DA,real_prices_max_DA, 'LineWidth',1.25)
+plot(LMP_time_DA, mean(case_data_LMP_DAM_raw(:,2:9),2), 'k','LineWidth',1.5)
+% plot(LMP_time_DA, min(case_data_LMP_DAM_raw(:,2:end),2), 'k','LineWidth',1.0)
+legend({'Actual (minimum)','Actual (maximum)','Simulation (Average)'})
+ylabel('LMP($/MWHr)')
+grid on
+% ylim([0,80])
+% yyaxis right
+% plot(LMP_time_DA, sum(case_data_PD_DAM_raw(:,2:end),2))
+title('Actual Versus Simulated LMPs: Day Ahead')
+hold off
+
+% figure()
+% yyaxis left
+% plot(time_real_RT,min_real_LMP_RT)
+% hold on
+% plot(time_real_RT,max_real_LMP_RT)
+% plot(LMP_time_RT,case_data_LMP(1,:,3))
+% ylabel('LMP($/MWHr)')
+% title('Real Time')
+% yyaxis right
+% plot(load_time,load_total)
+% legend({'min','max','Bus 2','load'})
+% hold off
+
+% figure()
+% yyaxis left
+% plot(time_real_DA,min_real_LMP_DA)
+% hold on
+% plot(time_real_DA,max_real_LMP_DA)
+% plot(LMP_time_DA,case_data_LMP_DAM_raw(1,:,3))
+% ylabel('LMP($/MWHr)')
+% title('Day Ahead')
+% yyaxis right
+% plot(load_time,load_total)
+% legend({'min','max','Bus 2','load'})
+% hold off
 
 
 
